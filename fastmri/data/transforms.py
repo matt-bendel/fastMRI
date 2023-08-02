e2edb7b628f94bc9417e84be54f777498cd1da5d
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-08-01 23:53:22 -0400

    GRO mask

--- fastmri/data/transforms.py
+++ fastmri/data/transforms.py
@@ -74,10 +74,17 @@ def apply_mask(
                 in the mask.
     """
     shape = (1,) * len(data.shape[:-3]) + tuple(data.shape[-3:])
-    mask, num_low_frequencies = mask_func(shape, offset, seed)
-    if padding is not None:
-        mask[..., : padding[0], :] = 0
-        mask[..., padding[1] :, :] = 0  # padding value inclusive on right of zeros
+    a = np.array(
+        [1, 23, 42, 60, 77, 92, 105, 117, 128, 138, 147, 155, 162, 169, 176, 182, 184, 185, 186, 187, 188, 189, 190,
+         191, 192, 193, 194, 195,
+         196, 197, 198, 199, 200, 204, 210, 217, 224, 231, 239, 248, 258, 269, 281, 294, 309, 326, 344, 363])
+
+    m = np.zeros((resolution, resolution))
+    m[:, a] = True
+    samp = m
+    mask = transforms.to_tensor(np.tile(samp, (data.shape[0], 1, 1)).astype(np.float32))
+    mask = torch.unsqueeze(mask, -1).repeat(1, 1, 1, 2)
+    num_low_frequencies = 16
 
     masked_data = data * mask + 0.0  # the + 0.0 removes the sign of the zeros
 
