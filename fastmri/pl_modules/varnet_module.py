8b12e6ae0c0ab46e6a9611a1a0f5f2959664381f
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-08-04 21:20:50 -0400

    Data range dbugging

--- fastmri/pl_modules/varnet_module.py
+++ fastmri/pl_modules/varnet_module.py
@@ -100,6 +100,9 @@ class VarNetModule(MriModule):
 
         target = fastmri.rss(fastmri.complex_abs(batch.target), dim=1)
 
+        target = target.unsqueeze(1)
+        output = output.unsqueeze(1)
+
 
         loss = self.loss(
             target.view(target.shape[0], -1, target.shape[2], target.shape[3]), output.view(output.shape[0], -1, output.shape[2], output.shape[3]),
