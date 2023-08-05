b4e2d71ecbdc9723eafb77ab1d2983b6be9948c2
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-08-04 20:30:56 -0400

    Data range dbugging

--- fastmri/pl_modules/varnet_module.py
+++ fastmri/pl_modules/varnet_module.py
@@ -93,11 +93,11 @@ class VarNetModule(MriModule):
         self.loss = pytorch_ssim.SSIM()
 
     def forward(self, masked_kspace, mask, num_low_frequencies):
-        print(masked_kspace[0, 0, 0, :, 0])
         return self.varnet(masked_kspace, mask, num_low_frequencies)
 
     def training_step(self, batch, batch_idx):
         output = self(batch.masked_kspace, batch.mask, batch.num_low_frequencies)
+        print(output[0, 0, 0, :, 0])
 
         target = batch.target
 
