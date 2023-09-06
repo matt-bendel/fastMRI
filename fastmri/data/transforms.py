fe6492144b61a55f0cf951b3ee46712e8d673af3
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-09-06 01:30:54 -0400

    random masks

--- fastmri/data/transforms.py
+++ fastmri/data/transforms.py
@@ -100,17 +100,44 @@ def apply_mask(
                 in the mask.
     """
     shape = (1,) * len(data.shape[:-3]) + tuple(data.shape[-3:])
-    a = np.array(
-        [1, 23, 42, 60, 77, 92, 105, 117, 128, 138, 147, 155, 162, 169, 176, 182, 184, 185, 186, 187, 188, 189, 190,
-         191, 192, 193, 194, 195,
-         196, 197, 198, 199, 200, 204, 210, 217, 224, 231, 239, 248, 258, 269, 281, 294, 309, 326, 344, 363])
-
     m = np.zeros((384, 384))
-    m[:, a] = True
-    samp = m
-    mask = to_tensor(np.tile(samp, (8, 1, 1)).astype(np.float32))
-    mask = torch.unsqueeze(mask, -1).repeat(1, 1, 1, 2)
-    num_low_frequencies = 16
+
+    # R=8 GRO
+    # a = np.array(
+    #     [1, 23, 42, 60, 77, 92, 105, 117, 128, 138, 147, 155, 162, 169, 176, 182, 184, 185, 186, 187, 188, 189, 190,
+    #      191, 192, 193, 194, 195,
+    #      196, 197, 198, 199, 200, 204, 210, 217, 224, 231, 239, 248, 258, 269, 281, 294, 309, 326, 344, 363])
+
+    # R=4 GRO
+    # a = np.array([0, 10, 19, 28, 37, 46, 54, 61, 69, 76, 83, 89, 95, 101, 107, 112, 118, 122, 127, 132, 136, 140, 144,
+    #      148,
+    #      151, 155, 158, 161, 164,
+    #      167, 170, 173, 176, 178, 181, 183, 186, 188, 191, 193, 196, 198, 201, 203, 206, 208, 211, 214, 217,
+    #      220,
+    #      223, 226, 229, 233, 236,
+    #      240, 244, 248, 252, 257, 262, 266, 272, 277, 283, 289, 295, 301, 308, 315, 323, 330, 338, 347, 356,
+    #      365,
+    #      374])
+    # m[:, a] = True
+    # samp = m
+    # mask = to_tensor(np.tile(samp, (8, 1, 1)).astype(np.float32))
+    # mask = torch.unsqueeze(mask, -1).repeat(1, 1, 1, 2)
+    # num_low_frequencies = 16
+
+    # Random masks...
+    x = [2, 8]
+    y = [32, 16]
+    r = np.random.randint(2, 9)
+    cw = int(np.rint(np.interp(r, x, y)))
+    if cw % 2 != 0:
+        cw = cw + 1
+    midway = resolution // 2
+    s = midway - cw // 2
+    e = s + cw
+    m[:, s:e] = True
+    a = np.random.choice(resolution - cw, resolution // r - cw, replace=False)
+    a = np.where(a < s, a, a + args.calib_width)
+    num_low_frequencies = cw
 
     masked_data = data * mask + 0.0  # the + 0.0 removes the sign of the zeros
 
